0bd596ff96d523a34f569d331176c4f3ada794ea
Jose Luis Rosado <jose.rosado.consultant@nielsen.com>
2021-11-10 19:02:41 -0600

    Add paginator

--- src/app/characteristics/characteristics.component.ts
+++ src/app/characteristics/characteristics.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, OnInit, ViewChild } from '@angular/core';
 import { CharsService } from '../service/chars.service';
 import { ControlAbstract } from '../class/control-abstract';
 import { provideMakersFunction, provideValidatorFunction } from '../utils/provide-makers.function';
@@ -6,6 +6,9 @@ import { CharacteristicModel } from '../model/characteristic.model';
 import { AbstractControl, FormArray, FormControl, FormGroup } from '@angular/forms';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatPaginator } from '@angular/material/paginator';
+import { BehaviorSubject, Subject } from 'rxjs';
+import { filter, pluck } from 'rxjs/operators';
+
 
 
 @Component({
@@ -16,26 +19,44 @@ import { MatPaginator } from '@angular/material/paginator';
     provideMakersFunction(CharacteristicsComponent)
   ]
 })
-export class CharacteristicsComponent extends ControlAbstract<CharacteristicModel[]>  implements OnInit {
+export class CharacteristicsComponent extends ControlAbstract<CharacteristicModel[]>  implements OnInit, AfterViewInit {
   displayedColumns = [ "selected", "id", "name", "notReportabled", "isElement", "alias"];
-  chars: any[];
-  frmChars: FormGroup;
+  chars: CharacteristicModel[];
+  frmMainChars: FormGroup;
   dataSource: MatTableDataSource<AbstractControl>;
-  @ViewChild('paginator', { static: true }) paginator!: MatPaginator;
+  @ViewChild('paginator', { static: false }) paginator: MatPaginator;
 
-  constructor(public charsService: CharsService) { super(); }
+  paginatorReady = new BehaviorSubject<boolean>(false);
 
-  ngOnInit(): void {  this.loadChars(); }
+  constructor(public charsService: CharsService, private cdr: ChangeDetectorRef,) { super(); }
+  ngAfterViewInit(): void {
+    if(this.paginator){
+      this.paginatorReady.next(true);
+      this.paginatorReady.complete();
+    }
+  }
+
+  ngOnInit(): void { this.loadChars();    }
+
 
   loadChars(): void {
     this.charsService.getChars().subscribe(chars =>{
-      this.frmChars = this.prepareForm(chars);
-      const frmArrayChars = this.frmChars.get('chars') as FormArray;
+      this.frmMainChars = this.prepareForm(chars);
+      const frmArrayChars = this.frmMainChars.get('chars') as FormArray;
       this.dataSource = new MatTableDataSource<AbstractControl>(frmArrayChars.controls);
-      this.dataSource.paginator = this.paginator;
       this.chars = chars;
-      this.frmChars.valueChanges.subscribe(event => this.setValue(event));
+      this.frmMainChars.valueChanges.subscribe(event => this.setValue(event));
+      if(!this.paginator)
+        this.paginatorReady.asObservable().pipe(filter(ready=>ready)).subscribe(()=>this.preparePaginator());
+      else{
+        this.preparePaginator();
+      }
     });
+  }
+
+  public preparePaginator(  ): void {
+    this.dataSource.paginator = this.paginator;
+    this.cdr.detectChanges();
   }
 
 
@@ -49,7 +70,7 @@ export class CharacteristicsComponent extends ControlAbstract<CharacteristicMode
   prepareFormArray(chars: CharacteristicModel[]): FormArray {
     return new FormArray( chars.map((char, i) => {
       const frmChar = this.prepareFrmChar(char);
-      frmChar.get('selected').valueChanges.subscribe(event=>this.selectCharEvent(event, char, frmChar));
+      frmChar.get('selected').valueChanges.pipe(pluck('chars')).subscribe(event=>this.selectCharEvent(event, char, frmChar));
       this.selectCharEvent(char.checked, char, frmChar);
       return frmChar;
     }));
@@ -57,7 +78,8 @@ export class CharacteristicsComponent extends ControlAbstract<CharacteristicMode
 
   prepareFrmChar(char: CharacteristicModel): FormGroup {
     return new FormGroup({
-      id: new FormControl({value: char.id, disabled: true}),
+      id: new FormControl( char.id),
+      name: new FormControl(char.name),
       selected: new FormControl(char.checked),
       notReportabled: new FormControl(false ),
       isElement: new FormControl(char.isElementByDefault),
